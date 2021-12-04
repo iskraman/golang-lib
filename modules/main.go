4f575bcee0ae453c3bbe130c8a67151571d59783
iskraman <iskraman@gmail.com>
2021-12-05 03:54:31 +0900

    syslog

--- modules/main.go
+++ modules/main.go
@@ -4,11 +4,16 @@ import (
 	"fmt"
 
 	"github.com/iskraman/golang-lib/modules/slice"
+	"github.com/iskraman/golang-lib/modules/syslog"
 )
 
 func main() {
 	items := []int{1, 2, 3, 4, 5, 6}
 	fmt.Println(slice.SliceExists(items, 5))   // returns true
 	fmt.Println(slice.SliceExists(items, "5")) // returns false
-	fmt.Println(slice.SliceExists(items, 10))  // returns false
+
+	syslog.DBG("%s %d", "log", 12)
+	syslog.ST("%s %d", "log", 34)
+	syslog.WARN("%s %d", "log", 56)
+	syslog.ERR("%s %d", "log", 78)
 }
